253c55d63b2cc92a20874465b66d460ac2d2eb34
Joseph <manueljosephtrabajo@gmail.com>
2022-10-05 16:50:41 -0600

    1- organize the repository. 2- add a file of study from w3

---
+++ java-file-of-documentation-W3/java_doc.java
@@ -0,0 +1,45 @@
+// ------------------------------------- //
+
+/*
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+
+        System.out.println("XXXXXXXX");
+    }
+}
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Joseph I am in Java");
+        System.out.println(true);
+
+        //type of variables
+        // String, int, float, boolean, char
+
+        String p = "Joseph";
+        System.out.println(p);
+
+        int o = 85;
+        System.out.println(o);
+
+        float i = 8.3f;
+        System.out.println(i);
+
+        //you can use final keyword if you don't want to modify the variable or nobody does.
+        final boolean y = true;
+        System.out.println(y);
+
+        char t = '#';
+        t = 'y';
+        System.out.println(t);
+
+        String h = "Joseph white";
+        System.out.println("Hello" + " " + h);
+
+
+    }
+}
+
+//- next lesson: Tipos de datos java -//
